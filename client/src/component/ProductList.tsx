ad2e47dc0668210bca682fbdf2f521e7e3584e58
sosojane <sosojane4ever@gmail.com>
2024-05-25 23:44:07 +0800

    chore: fix product list min width issue

--- client/src/component/ProductList.tsx
+++ client/src/component/ProductList.tsx
@@ -35,17 +35,18 @@ function ProductList({ products, onUpdate }: ProductListProps) {
     return (
         <div
             style={{
+                border: "1px solid #ccc",
                 display: "flex",
                 alignItems: "center",
-                justifyContent: "center",
-                marginTop: "10vh"
+                justifyContent: "left",
+                marginBottom: "5px",
+                borderRadius: "4px",
+                overflowX: "auto"
             }}
         >
             <div
                 style={{
-                    border: "1px solid #ccc",
-                    padding: "20px",
-                    borderRadius: "5px"
+                    padding: "20px"
                 }}
             >
                 <div>
@@ -62,71 +63,79 @@ function ProductList({ products, onUpdate }: ProductListProps) {
                     {products.length === 0 ? (
                         <div>No products</div>
                     ) : (
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th
-                                        style={{
-                                            textAlign: "left",
-                                            paddingRight: "10px"
-                                        }}
-                                    >
-                                        Name
-                                    </th>
-                                    <th
-                                        style={{
-                                            textAlign: "left",
-                                            padding: "10px"
-                                        }}
-                                    >
-                                        Quantity
-                                    </th>
-                                    <th
-                                        style={{
-                                            textAlign: "left",
-                                            padding: "10px"
-                                        }}
-                                    >
-                                        Amount
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {products.map((product, index) => (
-                                    <tr key={index}>
-                                        <td style={{ paddingRight: "10px" }}>
-                                            {product.name}
-                                        </td>
-                                        <td style={{ padding: "10px" }}>
-                                            <input
-                                                type="number"
-                                                value={product.quantity}
-                                                min="0"
-                                                onChange={(e) =>
-                                                    handleQuantityChange(
-                                                        index,
-                                                        parseInt(e.target.value)
-                                                    )
-                                                }
-                                            />
-                                        </td>
-                                        <td style={{ padding: "10px" }}>
-                                            <input
-                                                type="number"
-                                                value={product.amount}
-                                                min="0"
-                                                onChange={(e) =>
-                                                    handleAmountChange(
-                                                        index,
-                                                        parseInt(e.target.value)
-                                                    )
-                                                }
-                                            />
-                                        </td>
+                        <div style={{ width: "100%", tableLayout: "fixed" }}>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th
+                                            style={{
+                                                textAlign: "left",
+                                                paddingRight: "10px"
+                                            }}
+                                        >
+                                            Name
+                                        </th>
+                                        <th
+                                            style={{
+                                                textAlign: "left",
+                                                padding: "10px"
+                                            }}
+                                        >
+                                            Quantity
+                                        </th>
+                                        <th
+                                            style={{
+                                                textAlign: "left",
+                                                padding: "10px"
+                                            }}
+                                        >
+                                            Amount
+                                        </th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    {products.map((product, index) => (
+                                        <tr key={index}>
+                                            <td
+                                                style={{ paddingRight: "10px" }}
+                                            >
+                                                {product.name}
+                                            </td>
+                                            <td style={{ padding: "10px" }}>
+                                                <input
+                                                    type="number"
+                                                    value={product.quantity}
+                                                    min="0"
+                                                    onChange={(e) =>
+                                                        handleQuantityChange(
+                                                            index,
+                                                            parseInt(
+                                                                e.target.value
+                                                            )
+                                                        )
+                                                    }
+                                                />
+                                            </td>
+                                            <td style={{ padding: "10px" }}>
+                                                <input
+                                                    type="number"
+                                                    value={product.amount}
+                                                    min="0"
+                                                    onChange={(e) =>
+                                                        handleAmountChange(
+                                                            index,
+                                                            parseInt(
+                                                                e.target.value
+                                                            )
+                                                        )
+                                                    }
+                                                />
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     )}
                 </div>
             </div>
